c34f880ded451e864050142e4e156f31adab6b66
Bielz <gabriel.rocha.silva777@gmail.com>
2024-10-17 18:01:25 -0300

    Add files via upload
    first experience Python

---
+++ Atividade_AULA 03.py
@@ -0,0 +1,25 @@
+# foi atribuido a numero o 10
+
+# dado do tipo interger(inteiro)
+numero = 10
+print(numero)
+
+# dado do tipo string(texto)
+texto = 'isso é um texto'
+print(texto)
+
+# dado do tipo float(real)
+real = 10.5
+print(real)
+
+#dado do tipo bool(logico)
+boleano = True
+print(boleano)
+
+num1 = 2
+num2 = 5
+resultado = 7
+print(num1)
+print(num2)
+print('5+2=')
+print(resultado)
